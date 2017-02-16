68af192220e0eb157fb2029f4c0e17547d0ac8f0
Yi He <hey11@fudan.edu.cn>
2017-02-15 22:42:37 -0600

    Yi He's modification.

--- http_test.py
+++ http_test.py
@@ -2,6 +2,7 @@ import argparse
 import subprocess
 import requests
 import time
+from test.support import resource
 
 
 STATIC_RESOURCES = 'www'
@@ -24,6 +25,15 @@ class HttpTester(object):
         '/cats': 'http://en.wikipedia.org/wiki/Cat',
         '/uchicago/cs': 'http://www.cs.uchicago.edu/',
     }
+
+
+    """
+    List of non-existed objects
+    """
+    non_existed_objects = [
+        '/indeex.html',
+        '/foo/barr.html',
+    ]
 
     def __init__(self, server_class, server_port):
         self.server_class = server_class
@@ -51,20 +61,63 @@ class HttpTester(object):
     def check_expected_resources(self):
         for resource in self.expected_resources:
             try:
-                res = requests.get('{}/{}'.format(self.http_host, resource))
+                res = requests.get('{}{}'.format(self.http_host, resource))
                 assert res.status_code == 200
             except IOError as e:
                 print('Got io error in getting expected resources')
                 print('Error: {}'.format(str(e)))
-
+        print('Success! Checking expected resources.')
+        
+        
     def check_expected_redirects(self):
         for redirect in self.expected_redirects:
             try:
-                res = requests.get('{}/{}'.format(self.http_host, redirect))
+                res = requests.head('{}{}'.format(self.http_host, redirect))
                 assert res.status_code == 301
             except IOError as e:
                 print('Got io error in redirect')
                 print('Error: {}'.format(str(e)))
+        print('Success! Checking redirect resources.')
+        
+    def check_non_existed_objects(self):
+        for object in self.non_existed_objects:
+            try:
+                res = requests.get('{}{}'.format(self.http_host, object))
+                assert res.status_code == 404
+            except IOError as e:
+                print('Got io error in non-existed objects')
+                print('Error:{}'.format(str(e)))
+        print('Success! Checking non-existed objects.')
+                
+    def check_post_request(self):
+        for resource in self.expected_resources:
+            try:
+                res = requests.post('{}{}'.format(self.http_host, resource))
+                assert res.status_code == 403
+            except IOError as e:
+                print('Got io error in POST requests')
+                print('Error:{}'.format(str(e)))
+        print('Success! Checking post requests.')
+                
+    def check_head_request(self):
+        for resource in self.expected_resources:
+            try:
+                res = requests.head('{}{}'.format(self.http_host, resource))
+                assert res.status_code == 200
+            except IOError as e:
+                print('Got io error in HEAD requests')
+                print('Error:{}'.format(str(e)))
+                        
+        for redirect in self.expected_redirects:
+            try:
+                res = requests.head('{}{}'.format(self.http_host, redirect))
+                assert res.status_code == 301
+            except IOError as e:
+                print('Got io error in HEAD requests')
+                print('Error: {}'.format(str(e)))
+                        
+        print('Success! Checking head requests.')
+
 
     def destroy_server(self):
         self.server_process.kill()
@@ -80,7 +133,10 @@ tester = HttpTester(
     args.server_class,
     args.server_port,
 )
-tester.create_server()
+#tester.create_server()
 tester.check_expected_resources()
 tester.check_expected_redirects()
-tester.destroy_server()
+tester.check_non_existed_objects()
+tester.check_post_request()
+tester.check_head_request()
+#tester.destroy_server()
